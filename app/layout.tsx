38ec041eca62069598173a9cf041fb77854c52dc
Senlinmusi <senlinmusi80@gmail.com>
2026-06-23 16:25:47 +0800

    更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "俺儿乒乓球",
   description: "决战",
    icons: {
-    icon: 'https://ibb.co/TqtPqn6B', 
+    icon: 'https://i.ibb.co/S7BV71bX/1782201702149.png', 
   },
 };
 
